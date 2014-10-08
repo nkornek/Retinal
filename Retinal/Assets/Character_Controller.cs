a24f7dbbc0c728a7a7e64fac41f104b32765552c
nkornek <nkornek@gmail.com>
2014-10-07 22:12:08 -0400

    initial commit with basic gameplay test

---
+++ Retinal/Assets/Character_Controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Controller : MonoBehaviour {
+	public EyeballScript eyeball;
+	public Transform eyeballTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			eyeball.FireBall();
+		}
+
+		if (!eyeball.attached & Vector3.Distance(transform.position, eyeballTransform.position) < 2)
+		{
+			print ("test");
+			if (Input.GetMouseButton(1))
+			{
+				eyeball.PickUp();
+			}
+		}
+	
+	}
+}
---
+++ Retinal/Assets/EyeballScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeballScript : MonoBehaviour {
+	public bool attached;
+	public Transform parentTransform;
+
+	// Use this for initialization
+	void Start () {
+		attached = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (attached)
+		{
+			transform.position = parentTransform.position;
+			transform.rotation = parentTransform.rotation;
+		}
+	
+	}
+
+	public void FireBall()
+	{
+		if (attached)
+		{
+			attached = false;
+			transform.parent = null;
+			rigidbody.AddRelativeForce(Vector3.forward * 500);
+			rigidbody.AddRelativeForce(Vector3.up * 250);
+		}
+	}
+
+	public void PickUp()
+	{
+		attached = true;
+		transform.parent = parentTransform;
+	}
+}
